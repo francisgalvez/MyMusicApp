28152cd322b4100ec224ab1be1be4e93d4dbf7ac
francisgalvez <francisgalvezlao@gmail.com>
2017-06-07 20:58:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/mymusicapp/ArtistsActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.mymusicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ArtistsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artists);
+
+        // Find the View that shows the like button
+        ImageButton likeButton = (ImageButton) findViewById(R.id.like_button);
+        // Set a click listener on that View
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent likeButtonIntent = new Intent(ArtistsActivity.this, FavoritesActivity.class);
+
+                // Start the new activity
+                startActivity(likeButtonIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.mymusicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the "all songs" category
+        ImageButton allSongs = (ImageButton) findViewById(R.id.all_songs);
+        // Set a click listener on that View
+        allSongs.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(songsIntent);
+            }
+        });
+
+        // Find the View that shows the "artists" category
+        ImageButton artists = (ImageButton) findViewById(R.id.artists);
+        // Set a click listener on that View
+        artists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent artistsIntent = new Intent(MainActivity.this, ArtistsActivity.class);
+
+                // Start the new activity
+                startActivity(artistsIntent);
+            }
+        });
+
+        // Find the View that shows the "genres" category
+        ImageButton genres = (ImageButton) findViewById(R.id.genres);
+        // Set a click listener on that View
+        genres.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent genresIntent = new Intent(MainActivity.this, GenresActivity.class);
+
+                // Start the new activity
+                startActivity(genresIntent);
+            }
+        });
+
+        // Find the View that shows the "albums" category
+        ImageButton albums = (ImageButton) findViewById(R.id.albums);
+        // Set a click listener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+
+                // Start the new activity
+                startActivity(albumsIntent);
+            }
+        });
+
+        // Find the View that shows the like button
+        ImageButton likeButton = (ImageButton) findViewById(R.id.like_button);
+        // Set a click listener on that View
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent likeButtonIntent = new Intent(MainActivity.this, FavoritesActivity.class);
+
+                // Start the new activity
+                startActivity(likeButtonIntent);
+            }
+        });
+    }
+}
