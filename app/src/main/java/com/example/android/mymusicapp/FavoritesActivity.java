a2d9f590fbda92075181b18c6687fc4285c23e2c
francisgalvez <francisgalvezlao@gmail.com>
2017-06-08 17:46:15 +0200

    Changes to fit the project into the project's rubric.

--- app/src/main/java/com/example/android/mymusicapp/FavoritesActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/FavoritesActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.mymusicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -9,5 +13,50 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        // Find the View that shows the like button
+        ImageButton likeButton = (ImageButton) findViewById(R.id.like_button);
+        // Set a click listener on that View
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent likeButtonIntent = new Intent(FavoritesActivity.this, FavoritesActivity.class);
+
+                // Start the new activity
+                startActivity(likeButtonIntent);
+            }
+        });
+
+        // Find the View that shows the Main Menu button
+        Button mainMenuButton = (Button) findViewById(R.id.back_main_button);
+        // Set a click listener on that View
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link MainActivity}
+                Intent mainMenuButtonIntent = new Intent(FavoritesActivity.this, MainActivity.class);
+
+                // Start the new activity
+                startActivity(mainMenuButtonIntent);
+            }
+        });
+
+        // Find the View that shows the All Songs button
+        Button allSongsButton = (Button) findViewById(R.id.all_songs_button);
+        // Set a click listener on that View
+        allSongsButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link SongsActivity}
+                Intent allSongsButtonIntent = new Intent(FavoritesActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(allSongsButtonIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/mymusicapp/GenresActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/GenresActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class GenresActivity extends AppCompatActivity {
@@ -25,6 +26,36 @@ public class GenresActivity extends AppCompatActivity {
 
                 // Start the new activity
                 startActivity(likeButtonIntent);
+            }
+        });
+
+        // Find the View that shows the Main Menu button
+        Button mainMenuButton = (Button) findViewById(R.id.back_main_button);
+        // Set a click listener on that View
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link MainActivity}
+                Intent mainMenuButtonIntent = new Intent(GenresActivity.this, MainActivity.class);
+
+                // Start the new activity
+                startActivity(mainMenuButtonIntent);
+            }
+        });
+
+        // Find the View that shows the All Songs button
+        Button allSongsButton = (Button) findViewById(R.id.all_songs_button);
+        // Set a click listener on that View
+        allSongsButton.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link SongsActivity}
+                Intent allSongsButtonIntent = new Intent(GenresActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(allSongsButtonIntent);
             }
         });
     }
